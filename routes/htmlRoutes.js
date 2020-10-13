eb608ba75b784c810a88a2b12f47744650ece00d
Crystal <crystalwatkins428@gmail.com>
2020-10-13 17:14:52 -0400

    working to update routes on back end

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,15 +16,15 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "../client/public/tables.html"));
   });
 
   app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    res.sendFile(path.join(__dirname, "../client/public/reserve.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
 };
