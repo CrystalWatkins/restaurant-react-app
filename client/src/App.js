a22a0ad490d0a481c0a9605a0fcc766b28f08a8a
Crystal <crystalwatkins428@gmail.com>
2020-10-16 17:08:42 -0400

    updated app.js file and index.js file while building out the containers files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+import Home from "./containers/Home/Home";
+import Tables from "./containers/Tables/Table";
+import Waitlist from "./containers/Waitlist/Waitlist"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-          Hot Restaurant App
-      </header>
-    </div>
+  <Router>
+    <Switch>
+      <div className = "App">
+        <Route exact path = "/" component = {Home} />
+        <Route exact path = "/tables" component = {Tables} />
+        <Route exact path = "/waitlist" component = {Waitlist} />
+      </div>
+    </Switch>
+  </Router>
   );
 }
 
