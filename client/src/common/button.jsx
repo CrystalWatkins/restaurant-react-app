ea7293ae07218dcd4105eeb9885e5a9213c5454c
Crystal <crystalwatkins428@gmail.com>
2020-10-20 21:05:49 -0400

    update button.jsx file

---
+++ client/src/common/button.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const button = ({ route, btnClass, spanClass, btnText}) => {
+    let newBtnClass = "btn btn-lg";
+    let newSpanClass = "fa";
+
+    newBtnClass += btnClass;
+    newSpanClass += spanClass;
+
+    return (
+        <Link to={route}>
+            <button className={newBtnClass}>
+                <span className={newSpanClass}></span>
+                {btnText}
+            </button>
+        </Link>
+    );
+};
+
+
+export default button;
