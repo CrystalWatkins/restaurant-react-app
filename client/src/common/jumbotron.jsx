36d3f6cae2c1bf1434126ae025b9059425f01a07
Crystal <crystalwatkins428@gmail.com>
2020-10-23 14:26:28 -0400

    updated jumbotron.jsx

---
+++ client/src/common/jumbotron.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Button from "./button";
+
+const Jumbotron = ({ text, buttons, renderClearBtn, onDelete}) => {
+    console.log("jumbotron render clear button", renderClearBtn);
+    return (
+        <div className="jumbotron">
+            <h1 className="text-center">
+                <span className= "fa fa-fire"></span> Hot Restaurant
+            </h1>
+            <hr />
+            <h2 className="text-center"></h2>
+                {buttons.map((button) => button.context())}
+                {renderClearBtn &&(
+                    <>
+                    <br />
+                    <button className="btn btn-danger mt-3"onClick{() => onDelete()}>
+                    clear
+                    </button>
+                    </>
+                )}     
+        </div>
+    );
+};
+
+export default Jumbotron;
