d70a2dd902fdec3ea6a89d207c6faab4ab65118c
Crystal <crystalwatkins428@gmail.com>
2020-10-30 11:08:16 -0400

    updated form.jsx file

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Jumbotron from "./common/jumbotron";
 import { renderButtons } from "../utils/renderButtons";
-import "./Home.css";
 
 const Home = (props) => {
     const text =
     "We only have 6 tables! Book your seat before they are all gone!";
+    
     return (
         <Jumbotron
         text= {text}
